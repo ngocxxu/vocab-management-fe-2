117faf952ce809ac04eac71dfb796cd2715050ba
ngocxxu <ngocquach43@gmail.com>
2025-11-21 21:33:23 +0700

    chore: remove comment explaining replay sampling rate

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -21,8 +21,6 @@ if (!process.env.NEXT_PUBLIC_SENTRY_DISABLED) {
     tracesSampleRate: 1,
 
     // Define how likely Replay events are sampled.
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
     replaysSessionSampleRate: 0.1,
 
     // Define how likely Replay events are sampled when an error occurs.
