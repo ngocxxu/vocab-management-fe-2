f7c3aab5b6ecd6d59a293e188d145e99e0434812
ngocxxu <ngocquach43@gmail.com>
2025-12-28 13:44:19 +0700

    refactor: update QUESTION_TYPE_OPTIONS in vocab-trainer

--- src/constants/vocab-trainer.ts
+++ src/constants/vocab-trainer.ts
@@ -2,12 +2,12 @@ import { EQuestionType } from '@/enum/vocab-trainer';
 
 export const QUESTION_TYPE_OPTIONS = [
   { value: EQuestionType.MULTIPLE_CHOICE, label: 'Multiple Choice' },
-  { value: EQuestionType.FLIP_CARD, label: 'Flip Card' },
   { value: EQuestionType.FILL_IN_THE_BLANK, label: 'Fill in the Blank' },
-  { value: EQuestionType.SHORT_ANSWER, label: 'Short Answer' },
-  { value: EQuestionType.TRUE_OR_FALSE, label: 'True/False' },
-  { value: EQuestionType.MATCHING, label: 'Matching' },
+  // { value: EQuestionType.SHORT_ANSWER, label: 'Short Answer' },
+  // { value: EQuestionType.TRUE_OR_FALSE, label: 'True/False' },
+  // { value: EQuestionType.MATCHING, label: 'Matching' },
   { value: EQuestionType.TRANSLATION_AUDIO, label: 'Translation Audio' },
+  { value: EQuestionType.FLIP_CARD, label: 'Flip Card' },
 ];
 
 export const getExamUrl = (trainerId: string, questionType: EQuestionType): string => {
